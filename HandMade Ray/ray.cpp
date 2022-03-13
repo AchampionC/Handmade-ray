b169b01d2b5f1b01a13dbb017a398383593a9fcb
zhaohelei <zhaohelei@linctex.com>
2022-03-13 16:58:50 +0800

    摄像机系统, 光线击中地面的代码

--- HandMade Ray/ray.cpp
+++ HandMade Ray/ray.cpp
@@ -1,8 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <float.h>
 #include "ray.h"
+
 typedef char unsigned u8;
 typedef short unsigned u16;
 typedef int unsigned u32;
@@ -65,6 +66,33 @@ internal image_u32 AllocateImage(u32 Width, u32 Height)
 
 	return Image;
 }
+
+internal v3 RayCast(world* World, v3 RayOrigin, v3 RayDirection)
+{
+	v3 Result = World->Materials[0].Color; // Black
+
+	f32 HitDistance = F32MAX;
+
+	for (u32 PlaneIndex = 0; PlaneIndex < World->PlaneCount; PlaneIndex++)
+	{
+		plane Plane = World->Planes[PlaneIndex];
+
+		f32 Tolerance = 0.0001f;
+		f32 Denom = Inner(RayDirection, Plane.N); // 用于判断 射线与平面平行的标志
+		if ((Denom < -Tolerance) || (Denom > Tolerance)) { // 判断是否平行
+			f32 ThisDistance = (-Plane.d - Inner(RayOrigin, Plane.N)) / Denom;
+
+			if ((ThisDistance > 0) && (ThisDistance < HitDistance)) // 反方向的distance砍掉， 因为射线不可能反方向击中物体
+			{
+				HitDistance = ThisDistance;
+				Result = World->Materials[Plane.MatIndex].Color;
+			}
+		}
+	}
+
+	return Result;
+}
+
 internal void WriteImage(image_u32 Image, const char* OutputFileName)
 {
 	u32 OutputPixelSize = GetTotalPixelSize(Image);
@@ -101,7 +129,7 @@ internal void WriteImage(image_u32 Image, const char* OutputFileName)
 
 int main(int argc, char** argv)
 {
-	printf("hello world\n");
+	printf("Raycastin' ...\n");
 
 
 	material Materials[2] = {};
@@ -114,24 +142,24 @@ int main(int argc, char** argv)
 	Plane.MatIndex = 1;
 
 	world World = {};
-	World.MaterialCount = 1;
+	World.MaterialCount = 2;
 	World.Materials = Materials;
 	World.PlaneCount = 1;
 	World.Planes = &Plane;
-	World.SphereCount = 1;
+	World.SphereCount = 0;
 	World.Spheres = 0;
 
-	//                  y'
-	//				   /\
-	//				    |
-	//				    |
-	//                  |
-	//                 .Cam------>z'
-	//				 /
-	//              /
-	//             \/
-	//			   x'
-	//
+	//                      (0, 0, 1)
+	//			  y'       /\
+	//			      \     |    x'
+	//			       \    |   /
+	//                   \  |  / 
+	//                    .Cam 
+	//				           \
+	//                          \
+ 	//                           \
+	//			                  z
+	//                
 	//       z
 	//   y   |
 	//    \  |
@@ -140,24 +168,49 @@ int main(int argc, char** argv)
 	// 
 	// 
 	//
-	v3 CameraP = V3(0, 10, 1); // camera pos
-	v3 CameraZ = NOZ(CameraP - V3(0, 0, 0)); // camera ray  NOZ means normalize   z'
-	v3 CameraX = NOZ(Cross(CameraZ, V3(0, 0, 1)));  // x'
+	v3 CameraP = V3(0, -10, 1); // camera pos
+	v3 CameraZ = NOZ(CameraP - V3(0, 0, 0)); // camera ray  NOZ means normalize   z' (0, 1, 0)
+	v3 CameraX = NOZ(Cross(V3(0, 0, 1), CameraZ));  // x' 
 	v3 CameraY = NOZ(Cross(CameraZ, CameraX));    // y'
 
 	u32 OutputWidth = 1280;
 	u32 OutputHeight = 720;
 	image_u32 Image = AllocateImage(1280, 720);
 
+	f32 FilmDist = 1.0f;
+	f32 FilmW = 1.0f;
+	f32 FilmH = 1.0f;
+	f32 HalfFilmW = 0.5f * FilmW;
+	f32 HalfFilmH = 0.5f * FilmH;
+	v3 FilmCenter = CameraP - FilmDist * CameraZ;
+
 	u32* Out = Image.Pixels;
 	for (u32 Y = 0; Y < Image.Height; ++Y)
+	{
+		f32 FilmY = -1.0f + 2.0f * ((f32) Y / (f32) Image.Height);
 		for (u32 X = 0; X < Image.Width; ++X)
 		{
-			*Out++ = (Y < 32) ? 0xFFFF0000 : 0xFF0000FF;
+			f32 FilmX = -1.0f + 2.0f * ((f32)X / (f32)Image.Width);
+
+			v3 FilmP = FilmCenter + FilmX * HalfFilmW * CameraX + FilmY * HalfFilmH * CameraY;
+			
+			v3 RayOrigin = CameraP;
+			v3 RayDirection = NOZ(FilmP - CameraP);
+			
+			v3 Color = RayCast(&World, RayOrigin, RayDirection);
+			
+			v4 BMPColor = V4(255.0f * Color, 255.0f);
+			u32 BMPValue = BGRAPack4x8(BMPColor);
+
+
+			*Out++ =  BMPValue; // (Y < 32) ? 0xFFFF0000 : 0xFF0000FF;
 		}
+	}
 
 
 	WriteImage(Image, "test.bmp");
+
+	printf("Done...\n");
 
 	return 0;
 }
---
+++ HandMade Ray/ray.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "raymath.h"
+typedef char unsigned u8;
+typedef short unsigned u16;
+typedef int unsigned u32;
+
+typedef char s8;
+typedef short s16;
+typedef int s32;
+typedef float f32;
+
+#define F32MAX FLT_MAX;
+#define F32MIN FLT_MIN; 
+#define Pi32 3.15159265359f
+#define Tau32 6.28318530717958647692f
+
+#define internal static
+
+
+struct material
+{
+	v3 Color;
+
+};
+
+// TODO(casey): These will probably be reorganized for SIMD :)
+struct plane
+{
+	u32 MatIndex; // Material Index;
+	v3 N; // normal
+	f32 d; // distance alone the normal?
+};
+
+struct sphere
+{
+	v3 p; // center
+	f32 r; // radius
+	u32 MatIndex;
+};
+
+struct world
+{
+	u32 MaterialCount;
+	material* Materials;
+
+	u32 PlaneCount;
+	plane* Planes;
+
+	u32 SphereCount;
+	sphere* Spheres;
+};
